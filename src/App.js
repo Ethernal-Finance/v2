e95bba3ca5ab5c822f1add2e091a44fb0e82d4f4
Ethernaldev1 <97334891+Ethernaldev1@users.noreply.github.com>
2023-04-25 18:03:59 -0400

    Modified CSS and classnames to be more fitting

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import Web3 from "web3";
 import "./Images/Logo2.png";
 import abi from "./abi.json";
 
-
 const contractAddress = "0x55Be3e1F81Fc6d5169882339EcC0Fb8460f91de1";
 const tokenAddress = "0x55Be3e1F81Fc6d5169882339EcC0Fb8460f91de1";
 const tokenSymbol = "CIA";
 const tokenDecimal = 18;
-
-
 
 function App() {
   const [web3, setWeb3] = useState(null);
@@ -18,7 +15,6 @@ function App() {
   const [balance, setBalance] = useState(null);
   const [reinvest, setReinvest] = useState(false);
   const [withdrawableDividends, setWithdrawableDividends] = useState(0);
-  
 
   useEffect(() => {
     async function connectToWeb3() {
@@ -31,14 +27,23 @@ function App() {
           setContract(contract);
           const accounts = await web3.eth.getAccounts();
           setAccount(accounts[0]);
-          const balanceInWei = await contract.methods.balanceOf(accounts[0]).call();
+          const balanceInWei = await contract.methods
+            .balanceOf(accounts[0])
+            .call();
           const balanceInEther = web3.utils.fromWei(balanceInWei, "ether");
           setBalance(balanceInEther);
-          const isReinvest = await contract.methods.isReinvest(accounts[0]).call();
+          const isReinvest = await contract.methods
+            .isReinvest(accounts[0])
+            .call();
           setReinvest(isReinvest);
-          const withdrawableDividendInWei = await contract.methods.withdrawableDividendOf(accounts[0]).call();
-        const withdrawableDividendInEther = web3.utils.fromWei(withdrawableDividendInWei, "ether");
-        setWithdrawableDividends(withdrawableDividendInEther);
+          const withdrawableDividendInWei = await contract.methods
+            .withdrawableDividendOf(accounts[0])
+            .call();
+          const withdrawableDividendInEther = web3.utils.fromWei(
+            withdrawableDividendInWei,
+            "ether"
+          );
+          setWithdrawableDividends(withdrawableDividendInEther);
         } catch (error) {
           console.error(error);
         }
@@ -88,48 +93,57 @@ function App() {
   }
 
   return (
-    <div id="wrapper">
-      <div id="headerwrap">
-        <div id="header">
-            <img className="header__logo" src={require("./Images/Logo2.png")} />
-        </div>
-        </div>
-        <div id="navigationwrap">
-        <div id="navigation">
-            <div className="toggle-container">
-          <div className="toggle-text">Ethfin</div>
-          <label className="toggle">
-            <input
-              type="checkbox"
-              onChange={toggleReinvest}
-              checked={reinvest}
-            />
-            <span className="knob"></span>
-          </label>
-          <div className="toggle-text">CIA</div>
-        </div>
-        </div>
-        </div>
-        <div id="contentwrap">
-        <div id="content">
-            <div className="flex-container">
-                <div className="flex-items"><div className="account">Account</div> {account}</div>
-                <div className="flex-items"> <div className="balance">Token Balance</div> {balance} CIA</div>
-                <div className="flex-items">
-              <div className="account">Rewards Balance</div> {withdrawableDividends} BNB
+    <>
+      <div className="bg-image"></div>
+
+      <div className="flexbox-container">
+        <div className="main-flexbox">
+          <div className="headerwrap">
+            <div className="header">
+              <img
+                className="header__logo"
+                src={require("./Images/Logo2.png")}
+              />
             </div>
+          </div>
+
+          <div className="togglewrap">
+            <div className="toggle-container">
+              <div className="toggle-text">Ethfin</div>
+              <label className="toggle">
+                <input
+                  type="checkbox"
+                  onChange={toggleReinvest}
+                  checked={reinvest}
+                />
+                <span className="knob"></span>
+              </label>
+              <div className="toggle-text">CIA</div>
+            </div>
+
+            <div className="contentwrap">
+              <div className="content">
+                <div className="flex-items">
+                  <div className="title">Account</div> {account}
+                </div>
+
+                <div className="flex-items">
+                  <div className="title">Token Balance</div> {balance} CIA
+                </div>
+
+                <div className="flex-items">
+                  <div className="title">Rewards Balance</div>{" "}
+                  {withdrawableDividends} BNB
+                </div>
               </div>
-              
-        </div>
-        </div>
-        <div id="footerwrap">
-        <div id="footer">
-            <div id="footer">
-        <div className="button-container">
+            </div>
+
+            <div className="buttonswrap">
+              <div className="button-container">
                 <button className="button" onClick={claim}>
                   Claim
                 </button>
-          
+
                 <button
                   className="button"
                   onClick={() => {
@@ -157,10 +171,11 @@ function App() {
                   Add $CIA to wallet
                 </button>
               </div>
+            </div>
+          </div>
         </div>
-        </div>
-        </div>
-    </div>
+      </div>
+    </>
   );
 }
 
